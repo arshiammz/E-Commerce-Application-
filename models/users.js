9d843b19835f37d5d03282def98854faba260589
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-18 18:30:03 +0330

    User model edited.

--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 3 },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 5 },
+    password: { type: String, required: true},
+    deliveryAddress: { type: String, required: true, minlength: 5},
     role: { type: String, enum: ["user", "admin"], default: "user" }
 });
 
