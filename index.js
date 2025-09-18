f3cc99ba4dbefde6ca8718bd3f021c05d1090df5
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-18 17:40:14 +0330

    Connection to Database

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/cartwish")
+.then(() => console.log("MongoDB Connected Successfully!!"))
+.catch((err) => console.log("MongoDB Connection Failed!", err));
+
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`)});
