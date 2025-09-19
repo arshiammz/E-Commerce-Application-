5f5cf365fd54bc0d448f9e159e382f2faac79c3d
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-20 01:08:09 +0330

    login method created for user route.

--- routes/users.js
+++ routes/users.js
@@ -37,8 +37,31 @@ router.post('/', async (req, res) => {
     });
 
     await newUser.save();
-    const token = jwt.sign({_id: newUser._id, name: newUser.name}, process.env.JWT_KEY,{expiresIn: "1d"})
+    const token = generateToken({_id: newUser._id, name: newUser.name});
     res.status(201).json(token);
 });
+
+router.post('/login', async (req, res) => {
+    //find user by email
+    const {email, password} = req.body;
+    const user = await User.findOne({email: email})
+    if (!user) {
+        return res.status(401).json({message: "Invalid crentials"});
+    }
+
+    // compare encrypted password
+    const validPasssword = await bcrypt.compare(password, user.password);
+    if (!validPasssword) {
+        return res.status(401).json({message: "Invalid crentials"});
+    }
+
+    // create jwt and send the response
+    const token =generateToken({_id: user._id, name: user.name});
+    res.json(token);
+});
+
+const generateToken = (data) => {
+    return jwt.sign(data, process.env.JWT_KEY, {expiresIn: "1d"});
+}
 
 module.exports = router;
