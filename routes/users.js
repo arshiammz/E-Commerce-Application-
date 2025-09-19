afae4dcb7a8ff0cb15c8f1e513e9d51b8e1013dc
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-19 19:09:12 +0330

    create user requset validator added using joi.

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
 const User = require('../models/users');
+
+
+const createUserSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    deliveryAddress: Joi.string().min(5).required()
+});
 
 
 router.post('/', async (req, res) => {
     const {name, email, password, deliveryAddress} = req.body;
-    const user = await User.findOne({email: email});
 
+    const joiValidation = createUserSchema.validate(req.body);
+    if(joiValidation.error) {
+        return res.status(400).json(joiValidation.error.details[0].message);
+    };
+
+    const user = await User.findOne({email: email});
     if(user){
         res.status(400).json({message: "user already exits."})
     }
-
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
