c3f55914187471a9beda66229de93bdd8756643a
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-20 23:53:03 +0330

    seting file filter and filter in multer.

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const multer = require('multer');
-const Category = require('../models/category')
+const Category = require('../models/category');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'upload/category')
+    },
+    filename: (req, file, cb) => {
+        const timeStamp = Date.now();
+        const originalName = file.originalname.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9.-]/, "");
+        cb(null, `${timeStamp}-${originalName}`);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif' ];
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(new Error("Invalid file type!"))
+    }
+}
 
 const upload = multer({
-    dest: "upload/category"
-})
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    }
+});
 
 router.post("/", upload.single("icon"), async (req, res) => {
     if (!req.body.name || !req.file) {
