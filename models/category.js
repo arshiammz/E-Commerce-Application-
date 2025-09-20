5b171252551e73d26c69611730bbf6280a14d11c
arshiammz <mohammadzade.arshia84@gmail.com>
2025-09-20 17:11:47 +0330

    category model created.

---
+++ models/category.js
@@ -0,0 +1,11 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true},
+    image: { type: String, required: true}
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
